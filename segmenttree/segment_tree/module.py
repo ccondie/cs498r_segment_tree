65ee728a2fb33e9f140e0d16cd683abe08271345
Clayton Condie <clayton.condie@gmail.com>
2016-12-01 18:04:08 -0700

    basic operation functions

--- segmenttree/segment_tree/module.py
+++ segmenttree/segment_tree/module.py
@@ -4,28 +4,72 @@ import math
 class SegmentTree(object):
     def __init__(self, size):
         self.size = size
-        self.tree_size = (2 ** (math.ceil(math.log(size, 2)) + 1)) - 1
-        self.tree = [0] * self.tree_size
+        self.tree_nodes = (2 ** (math.ceil(math.log(size, 2)) + 1)) - 1
+        self.tree = [0] * self.tree_nodes
+        self.max_leaf = 2 ** (math.ceil(math.log(size, 2)))
 
     def setValue(self, value, index):
         # change the value at the index
-        self.tree[index] = value
+        tree_index = self.get_tree_index(index)
+        self.tree[tree_index] = value
 
         # until we get to the root node
-        while index != 0:
+        while tree_index != 0:
             # get the parent of that node
-            index = self.parentIndex(index)
+            tree_index = self.parentIndex(tree_index)
             # update it to the sum of its children
-            self.tree[index] = self.tree[self.childLeftIndex(index)] + self.tree[self.childRightIndex(index)]
+            self.tree[tree_index] = self.tree[self.childLeftIndex(tree_index)] + self.tree[
+                self.childRightIndex(tree_index)]
 
     def getSum(self, indexLeft, indexRight):
-        pass
-
-    def get_tree_index(self, default_index):
+        cur_index = 0
         int_low = 0
         int_high = self.size - 1
-        while int_high - int_low != 0:
-            pass
+
+        return self.getSumNode(indexLeft, indexRight, cur_index, int_low, int_high)
+
+    def getSumNode(self, indexLeft, indexRight, tree_index, int_low, int_high):
+        # if there is no overlap
+        if int_high < indexLeft or int_low > indexRight:
+            return 0
+
+        # if the interval of the current node is inside (inclusively) the range we are looking for, then we return the value of the node
+        if int_high <= indexRight and int_low >= indexLeft:
+            return self.tree[tree_index]
+
+        # fallback case: If any part of the interval of the node we are currently at is outside the range we are searching for, we call this function on its two children.
+        int_len = int_high - int_low
+        mid_index_mod = math.floor(int_len / 2)
+
+        childLeft = self.childLeftIndex(tree_index)
+        childRight = self.childRightIndex(tree_index)
+
+        leftVal = self.getSumNode(indexLeft, indexRight, childLeft, int_low, int_low + mid_index_mod)
+        rightVal = self.getSumNode(indexLeft, indexRight, childRight, int_low + mid_index_mod + 1, int_high)
+
+        return leftVal + rightVal
+
+    def get_tree_index(self, default_index):
+        cur_index = 0
+        int_low = 0
+        int_high = self.size - 1
+        int_len = int_high - int_low
+
+        while int_len != 0:
+            # get the size of the interval represented at cur_index
+            mid_index_mod = math.floor(int_len / 2)
+            if default_index <= int_low + mid_index_mod:
+                # if the index we are searching for falls to the left of the interval split
+                cur_index = self.childLeftIndex(cur_index)
+                int_low = int_low
+                int_high = int_low + mid_index_mod
+            else:
+                # if the index we are searching for falls to the right on the interval split
+                cur_index = self.childRightIndex(cur_index)
+                int_low = int_low + mid_index_mod + 1
+                int_high = int_high
+            int_len = int_high - int_low
+        return cur_index
 
     def childLeftIndex(self, parentIndex):
         return parentIndex * 2 + 1
--- segmenttree/tests/module_tests.py
+++ segmenttree/tests/module_tests.py
@@ -9,6 +9,17 @@ class SegmentTree_Test(unittest.TestCase):
     """This is an example of a Testing class. You'll want to replace this comment with your own.
     """
 
+    def setUp(self):
+        self.def_seg = SegmentTree(8)
+        self.def_seg.setValue(5, 0)
+        self.def_seg.setValue(3, 1)
+        self.def_seg.setValue(2, 2)
+        self.def_seg.setValue(5, 3)
+        self.def_seg.setValue(7, 4)
+        self.def_seg.setValue(2, 5)
+        self.def_seg.setValue(1, 6)
+        self.def_seg.setValue(9, 7)
+
     def test_size(self):
         """Test that the size attribute exists
         """
@@ -24,17 +35,21 @@ class SegmentTree_Test(unittest.TestCase):
     '''
     MISC TESTS
     '''
+
     def test_single_setValue(self):
         seg = SegmentTree(10)
         seg.setValue(10, 5)
         self.assertEqual(seg.tree[0], seg.tree[5])
 
-    def test_double_setValue(self):
-        seg = SegmentTree(10)
-        print(seg.tree)
-        seg.setValue(10, 5)
-        print(seg.tree)
-        seg.setValue(7, 2)
-        print(seg.tree)
+    def test_get_tree_index(self):
+        seg = SegmentTree(8)
+        self.assertEqual(seg.get_tree_index(3), 10)
+        self.assertEqual(seg.get_tree_index(0), 7)
+        self.assertEqual(seg.get_tree_index(7), 14)
 
-        self.assertEqual(seg.tree[0], 17)
+    def test_double_setValue(self):
+        print(self.def_seg.tree)
+
+    def test_get_sum(self):
+        pass
+
